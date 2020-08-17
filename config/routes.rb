0b13211c03d644e62ad5e6f3dc5f37b9a845613b
hoang.quang.chinh <hoang.quang.chinh@sun-asterisk.com>
2020-08-18 01:09:37 +0700

    [TASK][27007] layout application

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  scope "(:locale)", locale: /vi|en/ do
+    root "static_pages#home"
+
+    get "/home", to: "static_pages#home"
+  end
 end
