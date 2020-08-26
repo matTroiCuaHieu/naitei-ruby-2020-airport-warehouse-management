87b1f88b8d3e4c994c4619962e598648c8380065
matTroiCuaHieu <nguyen.minh.hieu@sun-asterisk.com>
2020-08-26 11:29:02 +0700

    Fix scope, constant. Update form's layout.

--- app/javascript/packs/locations/location.js
+++ app/javascript/packs/locations/location.js
@@ -1,10 +1,14 @@
-$("#select").change(function(){
+const all_value = 1
+const radar_value = 2
+const ready_value = 3
+
+$( "#select" ).change( function() {
   $(".table-content").remove()
-  if($("#select option:selected")[0].value == "1"){
+  if($("#select option:selected")[0].value == all_value){
     $("#table-content-body").html("<%= j render @locations %>");
-  }else if($("#select option:selected")[0].value == "2"){
+  }else if($("#select option:selected")[0].value == radar_value){
     $("#table-content-body").html("<%= j render @radars %>");
-  }else if($("#select option:selected")[0].value == "3"){
+  }else if($("#select option:selected")[0].value == ready_value){
     $("#table-content-body").html("<%= j render @readys %>");
   }
 })
